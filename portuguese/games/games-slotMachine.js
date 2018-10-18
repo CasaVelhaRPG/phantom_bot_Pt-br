97c0e4f14b25c4e62fc23f8a7eed494758212c59
pablotq <36311760+pablotq@users.noreply.github.com>
2018-10-18 15:56:19 -0300

    Tradução do slotMachine.js

--- portuguese/games/games-slotMachine.js
+++ portuguese/games/games-slotMachine.js
@@ -1,6 +1,6 @@
 $.lang.register('slotmachine.result.start', '$1: [ $2 $3 $4 ] ');
 $.lang.register('slotmachine.result.win', ' +$1 ');
-$.lang.register('slotmachine.rewards.usage', 'Usage: !slot rewards [val1] [val2] [val3] [val4] [val5]. Currently: $1');
-$.lang.register('slotmachine.rewards.success', 'Updated rewards for the slot machine.');
-$.lang.register('slotmachine.emote.usage', 'Usage: !slot emotes [emote1] [emote2] [emote3] [emote4] [emote5]. Currently: $1');
-$.lang.register('slotmachine.emote.success', 'Updated emotes for the slot machine.');
+$.lang.register('slotmachine.rewards.usage', 'Uso: !slot premia [val1] [val2] [val3] [val4] [val5]. Atualmente: $1');
+$.lang.register('slotmachine.rewards.success', 'Recompensas atualizadas para o caça-níquel.');
+$.lang.register('slotmachine.emote.usage', 'Uso: !slot emotes [emote1] [emote2] [emote3] [emote4] [emote5]. Atualmente: $1');
+$.lang.register('slotmachine.emote.success', 'Emotes atualizados para o caça-níquel.');
