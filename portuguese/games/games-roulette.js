092dcb664a292fc96224f268f24d38b8e4e7e2b4
pablotq <36311760+pablotq@users.noreply.github.com>
2018-10-18 17:00:09 -0300

    tradução roulette.js

--- portuguese/games/games-roulette.js
+++ portuguese/games/games-roulette.js
@@ -1,20 +1,20 @@
-$.lang.register('roulette.win.1', 'The trigger is pulled, and the revolver clicks. $1 has lived to survive roulette!');
-$.lang.register('roulette.win.2', 'The trigger is pulled, but the revolver malfunctions! $1 is lucky, and live\'s another day!');
-$.lang.register('roulette.win.3', 'The trigger is pulled, but $1 has been saved by magic!');
-$.lang.register('roulette.win.4', 'The trigger is pulled, but $1 has lived due to not having anything in their skull to begin with.');
-$.lang.register('roulette.win.5', 'The trigger is pulled, but $1\'s skull was so thick the bullet could not penetrate it.');
-$.lang.register('roulette.win.6', 'The trigger is pulled, but $1 stopped space and time and dodged the bullet!');
-$.lang.register('roulette.win.7', 'The trigger is pulled, but $1 did not die from the bullet, the bullet died from them.');
+$.lang.register('roulette.win.1', 'O gatilho é puxado, e o revólver clica. $1 sobreviveu a roleta russa!');
+$.lang.register('roulette.win.2', 'O gatilho é puxado, mas o revólver emperra! $1 tem sorte e vive outro dia!');
+$.lang.register('roulette.win.3', 'O gatilho é puxado, mas $1 é salvo por mágica!');
+$.lang.register('roulette.win.4', 'O gatilho é puxado, mas $1 sobrevive por não ter nada no lugar do cérebro.');
+$.lang.register('roulette.win.5', 'O gatilho é puxado, mas $1 é tão cabeça dura que a bala não conseguiu perfurar seu crânio.');
+$.lang.register('roulette.win.6', 'O gatilho é puxado, mas $1 para o espaço-tempo e devia da bala!');
+$.lang.register('roulette.win.7', 'O gatilho é puxado, mas $1 não morreu da bala, a bala morreu dele.');
 
-$.lang.register('roulette.lost.1', 'The trigger is pulled, and the revolver fires! $1 lies dead in the chat.');
-$.lang.register('roulette.lost.2', 'The trigger is pulled, and $1 loses their head!');
-$.lang.register('roulette.lost.3', '$1\'s finger slides over the trigger, $1 crashes to the floor like a sack of flour!');
-$.lang.register('roulette.lost.4', 'The trigger is pulled, and the hand-cannon goes off with a roar! $1 lies dead in the chat.');
-$.lang.register('roulette.lost.5', 'The hammer drops, and the .44 fires! $1 is now dead in the chat.');
-$.lang.register('roulette.lost.6', 'The trigger is pulled, and the revolver fires! $1 is now just a blood splatter on the wall.');
-$.lang.register('roulette.lost.7', 'The trigger is pulled, and the revolver fires! $1 ended their miserable life.');
+$.lang.register('roulette.lost.1', 'O gatilho é puxado, e o revólver dispara! $1 cai morto no chat.');
+$.lang.register('roulette.lost.2', 'O gatilho é puxado, e $1 perde a cabeça!');
+$.lang.register('roulette.lost.3', 'O dedo de $1 escorrega no gatilho, $1 cai duro no chão!');
+$.lang.register('roulette.lost.4', 'O gatilho é puxado, e o disparo da arma ecoa! $1 cai morto no chat.');
+$.lang.register('roulette.lost.5', 'O .38 é disparado! $1 agora está morto no chat.');
+$.lang.register('roulette.lost.6', 'O gatilho é puxado, e o revólver dispara! $1 está por toda a parede.');
+$.lang.register('roulette.lost.7', 'O gatilho é puxado, e a arma dispara! $1 terminou sua vida miserável.');
 
-$.lang.register('roulette.set.timeouttime.success', 'The !roulette timeout has been set to $1 seconds!');
-$.lang.register('roulette.set.timeouttime.usage', 'Usage: !roulette timeout [seconds]');
-$.lang.register('roulette.timeout.notifyuser', 'You have lost the roulette and have been timed out for $1 seconds.');
-$.lang.register('roulette.console.loaded', 'Found Roulette messages: $1 wins, $2 losts.');
+$.lang.register('roulette.set.timeouttime.success', 'O tempo limite da !roulette é de $1 segundos!');
+$.lang.register('roulette.set.timeouttime.usage', 'Uso: !roulette timeout [segundos]');
+$.lang.register('roulette.timeout.notifyuser', 'Você perdeu a roleta russa e foi silenciado por $1 segundos.');
+$.lang.register('roulette.console.loaded', 'Mensagens da roleta encontradas: $1 vitórias, $2 derrotas.');
