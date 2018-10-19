e43f64714ce9d7f2f2bc7c701bd8181fb8536380
wesvian <44215520+wesvian@users.noreply.github.com>
2018-10-19 11:15:33 -0300

    Precisa de revisão

--- portuguese/systems/systems-quoteSystem.js
+++ portuguese/systems/systems-quoteSystem.js
@@ -1,19 +1,19 @@
-$.lang.register('quotesystem.add.usage1', 'Usage: !addquote [quote]');
-$.lang.register('quotesystem.add.usage2', 'Usage: !addquote [user] [quote]');
-$.lang.register('quotesystem.add.offline', 'Only moderators may add quotes when the stream is offline.');
-$.lang.register('quotesystem.add.success', 'Added new quote from $1 with id #$2.');
-$.lang.register('quotesystem.del.usage', 'Usage: !delquote [quote id]');
-$.lang.register('quotesystem.del.success', 'Deleted quote $1. There are $2 quotes now.');
-$.lang.register('quotesystem.del.404', 'Could not delete quote $1... Are you sure it exists?');
-$.lang.register('quotesystem.get.success', '[(id)] "(quote)", by (user) ((date))'); // Tags = (id) (quote) (user) (game) (date) //
-$.lang.register('quotesystem.get.404', 'Could not find quote $1... Are you sure it exists?');
-$.lang.register('quotesystem.edit.usage', 'Usage: !editquote [quote id] [user|game|quote] text...]');
-$.lang.register('quotesystem.edit.user.success', 'Updated user on quote $1 to $2.');
-$.lang.register('quotesystem.edit.game.success', 'Updated game on quote $1 to $2.');
-$.lang.register('quotesystem.edit.quote.success', 'Updated quote on quote $1 to $2.');
-$.lang.register('quotesystem.edit.404', 'Could not find quote $1... Are you sure it exists?');
-$.lang.register('quotesystem.quotemessage.usage', 'Usage: !quotemessage [message] (Tags: (id) (quote) (user) (game) (date))');
-$.lang.register('quotesystem.quotemessage.success', 'Changed the message used for quotes.');
-$.lang.register('quotesystem.searchquote.usage', 'Usage: !searchquote [text] (Must provide at least 5 characters)');
-$.lang.register('quotesystem.searchquote.404', 'No matching quotes found.');
+$.lang.register('quotesystem.add.usage1', 'Uso: !addPUN [PUN]');
+$.lang.register('quotesystem.add.usage2', 'Uso: !addPUN [usuário] [PUN]');
+$.lang.register('quotesystem.add.offline', 'Somente moderadores podem adicionar PUNs quando a stream estiver off-line.');
+$.lang.register('quotesystem.add.success', 'Adicionar novo PUN de $1 com id #$2.');
+$.lang.register('quotesystem.del.usage', 'Uso: !delPUN [PUN id]');
+$.lang.register('quotesystem.del.success', 'Deletar PUN $1. Tem $2 PUNs agora.');
+$.lang.register('quotesystem.del.404', 'Não foi possivel excluir o PUN $1... Você tem certeza que ele existe?');
+$.lang.register('quotesystem.get.success', '[(id)] "(PUN)", por (usuário) ((data))'); // Tags = (id) (PUN) (usuário) (jogo) (data) //
+$.lang.register('quotesystem.get.404', 'Não foi possivel encontar o PUN $1... Você tem certeza que ele existe?');
+$.lang.register('quotesystem.edit.usage', 'Uso: !editarPUN [PUN id] [usuário|jogo|PUN] texto...]');
+$.lang.register('quotesystem.edit.user.success', 'Atualizar PUN do usuário $1 para $2.');
+$.lang.register('quotesystem.edit.game.success', 'Atualizar PUN do jogo $1 para $2.');
+$.lang.register('quotesystem.edit.quote.success', 'Atualizar PUN do PUN $1 para $2.');
+$.lang.register('quotesystem.edit.404', 'Não foi possível encontrar o PUN $1... Você tem certeza que ele existe?');
+$.lang.register('quotesystem.quotemessage.usage', 'Usage: !PUN mensagem [mensagem] (Tags: (id) (PUN) (usuário) (jogo) (data))');
+$.lang.register('quotesystem.quotemessage.success', 'Alterou a mensagem usada no PUN.');
+$.lang.register('quotesystem.searchquote.usage', 'Uso: !Pesquisar PUN  [texto] (Deve fornecer pelo menos 5 caracteres)');
+$.lang.register('quotesystem.searchquote.404', 'Nenhum PUN correspondente foi encontrado.');
 $.lang.register('quotesystem.searchquote.found', 'Quote IDs with matches: $1');
