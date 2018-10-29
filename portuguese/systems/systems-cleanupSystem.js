1b2aba07896d5b350632c3879bb1b05607126291
pablotq <36311760+pablotq@users.noreply.github.com>
2018-10-29 17:03:25 -0300

    traduzido - pablotq

--- portuguese/systems/systems-cleanupSystem.js
+++ portuguese/systems/systems-cleanupSystem.js
@@ -1,3 +1,3 @@
-$.lang.register('cleanupsystem.run.progress', 'A limpeza está começando, $1 pode não responder ou ficar lento durante esse processo. Isso pode levar tempo ...');
+$.lang.register('cleanupsystem.run.progress', 'A limpeza está começando, $1 pode não responder ou ficar lento durante esse processo. Isso pode levar um tempo ...');
 $.lang.register('cleanupsystem.run.success', 'Limpeza concluída!');
 $.lang.register('cleanupsystem.run.usage', 'Uso: !cleanup [tempo / pontos / tudo] [quantidade de tempo em segundos ou pontos se limpando pontos]');
