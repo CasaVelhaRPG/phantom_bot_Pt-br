ce7338087df742c62f163cfbd9d6f10c93049c08
pablotq <36311760+pablotq@users.noreply.github.com>
2018-10-29 17:25:27 -0300

    traduzi raid como gank -pablotq
    traduzi raid como gank por ser o termo utilizado pela comunidade
    alterei a linha 3

--- portuguese/systems/systems-raidSystem.js
+++ portuguese/systems/systems-raidSystem.js
@@ -1,6 +1,6 @@
 $.lang.register('raidsystem.raid', 'Estamos gankando http://twitch.tv/$1 ! $2');
 $.lang.register('raidsystem.raid.usage', 'Uso: !raid [username]');
-$.lang.register('raidsystem.raided', 'Estamos sendo gankados por $1! Esta é a $2 vez que $1 ganka. Confiram o canal deles e clique no botão "Seguir" http://twitch.tv/$1 !');
+$.lang.register('raidsystem.raided', 'Estamos sendo gankados por $1! Esta é a $2 vez que $1 ganka nosso canal. Confiram o canal deles e clique no botão "Seguir" http://twitch.tv/$1 !');
 $.lang.register('raidsystem.raider.usage', 'Uso: !raider [username] -- !raider contagem [username]');
 $.lang.register('raidsystem.message.nomessageset', '$1 ainda não definiu uma mensagem de gank! Use "!setraidmsg [messagem...]" para definir uma.');
 $.lang.register('raidsystem.message.usage', 'Uso "!setraidmsg [messagem...]" para definir uma mensagem.');
