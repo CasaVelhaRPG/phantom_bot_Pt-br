7d787ef5288ee48df1419314d8a659181783655a
pablotq <36311760+pablotq@users.noreply.github.com>
2018-10-29 17:23:39 -0300

    traduzi raid como gank -pablotq
    traduzi raid como gank porque é o termo utilizado pela comunidade

--- portuguese/systems/systems-raidSystem.js
+++ portuguese/systems/systems-raidSystem.js
@@ -1,8 +1,8 @@
-$.lang.register('raidsystem.raid', 'We are raiding http://twitch.tv/$1 ! $2');
-$.lang.register('raidsystem.raid.usage', 'Usage: !raid [username]');
-$.lang.register('raidsystem.raided', 'We are being raided by $1! This is the $2 time $1 has raided. Go check out their channel and hit that follow button at http://twitch.tv/$1 !');
-$.lang.register('raidsystem.raider.usage', 'Usage: !raider [username] -- !raider count [username]');
-$.lang.register('raidsystem.message.nomessageset', '$1 hasn\'t set a raid message yet! Use "!setraidmsg [message...]" to set one.');
-$.lang.register('raidsystem.message.usage', 'Usage "!setraidmsg [message...]" to set one.');
-$.lang.register('raidsystem.message.setsuccess', 'The new raid message is "$1".');
-$.lang.register('raidsystem.console.announceraidmsg', 'The current raid message is "$1".');
+$.lang.register('raidsystem.raid', 'Estamos gankando http://twitch.tv/$1 ! $2');
+$.lang.register('raidsystem.raid.usage', 'Uso: !raid [username]');
+$.lang.register('raidsystem.raided', 'Estamos sendo gankados por $1! Esta é a $2 vez que $1 ganka. Confiram o canal deles e clique no botão "Seguir" http://twitch.tv/$1 !');
+$.lang.register('raidsystem.raider.usage', 'Uso: !raider [username] -- !raider contagem [username]');
+$.lang.register('raidsystem.message.nomessageset', '$1 ainda não definiu uma mensagem de gank! Use "!setraidmsg [messagem...]" para definir uma.');
+$.lang.register('raidsystem.message.usage', 'Uso "!setraidmsg [messagem...]" para definir uma mensagem.');
+$.lang.register('raidsystem.message.setsuccess', 'A nova mensagem de gank é "$1".');
+$.lang.register('raidsystem.console.announceraidmsg', 'A mensagem atual de gank é "$1".');
