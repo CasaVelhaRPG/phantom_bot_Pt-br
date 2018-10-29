86725218f8f6019a561b6a650b1eff9b21f538cb
pablotq <36311760+pablotq@users.noreply.github.com>
2018-10-29 17:12:23 -0300

    mantive a palavra Cooldown- pablotq

--- portuguese/systems/systems-greetingSystem.js
+++ portuguese/systems/systems-greetingSystem.js
@@ -1,9 +1,9 @@
-$.lang.register('greetingsystem.set.autogreet.enabled', 'Auto Greeting Enabled. $1 will now greet viewers with a greeting configured.');
-$.lang.register('greetingsystem.set.autogreet.disabled', 'Auto Greeting Disabled');
-$.lang.register('greetingsystem.set.autogreet.noname', 'Please use the (name) tag in the default greeting.');
-$.lang.register('greetingsystem.set.personal.success', 'Changed your personal greeting to "$1".');
-$.lang.register('greetingsystem.remove.personal.success', 'Successfully removed your personal greeting message.');
-$.lang.register('greetingsystem.generalusage.admin', 'Usage: !greeting [toggle | enable [default | message text] | disable ]. Tags for message text: (name)');
-$.lang.register('greetingsystem.generalusage.other', 'Usage: !greeting [ enable [default | message text] | disable ]. Tags for message text: (name)');
-$.lang.register('greetingsystem.cooldown.usage', 'Usage: !greeting cooldown [hours]');
-$.lang.register('greetingsystem.cooldown.success', 'Greetings cooldown set to $1 hours.');
+$.lang.register('greetingsystem.set.autogreet.enabled', 'Saudação Automática Habilitada. $1 agora cumprimentará os espectadores com uma saudação configurada.');
+$.lang.register('greetingsystem.set.autogreet.disabled', 'Saudação Automática Desativada');
+$.lang.register('greetingsystem.set.autogreet.noname', 'Por favor, use a tag (nome) na saudação padrão.');
+$.lang.register('greetingsystem.set.personal.success', 'Mudou sua saudação pessoal para "$1".');
+$.lang.register('greetingsystem.remove.personal.success', 'Removeu com sucesso sua mensagem de saudação pessoal.');
+$.lang.register('greetingsystem.generalusage.admin', 'Uso: !greeting [alternar | ativar [default | mensagem de texto] | desativar ]. Tags para o texto da mensagem: (nome)');
+$.lang.register('greetingsystem.generalusage.other', 'Uso: !greeting [ ativar [default | mensagem de texto] | desativar ]. Tags para o texto da mensagem: (nome)');
+$.lang.register('greetingsystem.cooldown.usage', 'Uso: !greeting cooldown [horas]');
+$.lang.register('greetingsystem.cooldown.success', 'Cooldown das saudações definido como $1 horas.');
